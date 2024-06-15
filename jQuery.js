69c8de197344a5209abe9c68948cded6665d51da
Denis Menace <trideks@sbb.rs>
2024-06-15 14:28:29 +0200

    reservations table added

--- jQuery.js
+++ jQuery.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
   loadCourses();
+  $("#reservations_table_container").toggle();
+
   document.getElementById("forma").addEventListener("submit", function (event) {
     event.preventDefault();
-    //manageEmployee(event);
+    manageReservations(event);
   });
 });
+let btnText = "View Reservations";
+
+$("#btn_toggle").text(btnText);
 function loadCourses() {
   let getCourses = $.ajax({
     type: "GET",
@@ -49,6 +54,93 @@ function makeReservation(id, event) {
     }
   );
   inputModal.show();
+}
+function manageReservations(event) {
+  let formData = new FormData(event.target);
+  //let jsonToSend = convertToJson(event.target);
+  //if ($("#forma").valid()) {
+  if (event.target) {
+    let reservation = Object.fromEntries(formData);
+    let isEdit = reservation.id;
+    if (isEdit) {
+      $.ajax({
+        url: "http://localhost:3000/reservations/" + reservation.id,
+        type: "PUT",
+        dataType: "json",
+        data: JSON.stringify(reservation),
+        success: function (data) {
+          loadReservations();
+          $("#modal2").modal("toggle");
+        },
+        error: function () {
+          alert("Neuspelo editovanje");
+        },
+      });
+    } else {
+      $.ajax({
+        url: "http://localhost:3000/reservations",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify(reservation),
+        success: function (element) {
+          loadCourses();
+          $("#modalUnos").modal("toggle");
+        },
+        error: function () {
+          alert("Neuspelo dodavanje");
+        },
+      });
+    }
+  } else {
+    alert("Popuni formu pravilno");
+  }
+}
+function loadReservations() {
+  let getRequest = $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/reservations",
+  });
+
+  getRequest.done(function (data) {
+    $("#employee-table tbody").empty();
+    let prices = [];
+    $.each(data, function (i, element) {
+      prices.push(element.price);
+      $("#reservations-table tbody").append(`<tr>
+        <td>${element.name}</td>
+        <td>${element.surname}</td>
+        <td>${element.email}</td>
+        <td>${element.courseName}</td>
+        <td>${element.price}</td>
+        <td>${element.comment}</td>
+        <td>
+            <button id="${element.id}_edit" class="btn btn-warning" onclick="editEmployee(${element.id}, event)">Izmena</button>
+        </td>
+        <td>
+            <button id="${element.id}_delete" class="btn btn-danger" onclick="deleteEmployee(${element.id}, event)">Obrisi</button>
+        </td>
+        </tr>`);
+    });
+    let pricesSum = prices.reduce(function (total, num) {
+      return parseInt(total) + parseInt(num);
+    });
+    $("#calculation").text(`${pricesSum}$`);
+  });
+
+  getRequest.fail(function (err) {
+    alert(err.statusText);
+  });
+}
+function toggleTables() {
+  loadReservations();
+  if (btnText === "View Reservations") {
+    btnText = "Back to Courses";
+  } else {
+    btnText = "View Reservations";
+  }
+  $("#btn_toggle").text(btnText);
+  $("#main_table_container").toggle();
+  $("#reservations_table_container").toggle();
 }
 function closeModal() {
   $("#modal2").modal("toggle");
